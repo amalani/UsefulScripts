539f5899e16647022b94b25d50dc5728991f7750
Abhishek Malani <malani@gmail.com>
2016-10-26 12:04:14 -0700

    Better help

--- GmailPurge/GmailPurge.js
+++ GmailPurge/GmailPurge.js
@@ -1,13 +1,25 @@
-var EMAIL = "";
-var GMAIL_LABEL = "";
-var PURGE_AFTER = "30";
+/*
+Script that lets you use GoogleScript engine to filter for emails and delete
+specific emails.
+1. Go to script.google.com.
+2. Provide authorization to access your Google a/c.
+3. Create a new project.
+4. Paste this script.
+5. Update the variables at the start of the script.
+6. Run. 
+*/
 
-# Search filter to retrieve threads.
+// Where to send notifications
+var EMAIL = "";
+// Delay - skip the most recent x days of emails. Use 0 to not skip anything.
+var PURGE_AFTER = 30;
+
+// Search filter to retrieve threads.
 var search_filter = "label:label_to_delete"
-# Once you have the threads, I only wanted to delete emails sent by the following person. 
+// Once you have the threads, I only wanted to delete emails sent by the following person. 
 var filter1 = { from: 'donotreply@us.gov' }
 
-// --- Copy below this line into script ---
+// --- do not change below this ---
 
 function Email(email_subject, body) {
     body.push("<br><br>" + MailApp.getRemainingDailyQuota() + " more emails can be sent today.");
@@ -20,17 +32,21 @@ function Email(email_subject, body) {
 }
 
 function purgeGmail() {
-    var age = new Date();
-    age.setDate(age.getDate() - PURGE_AFTER);
-
-    var purge_date  = Utilities.formatDate(age, Session.getScriptTimeZone(), "yyyy-MM-dd");
+    var search = search_filter;
+    
+    if (PURGE_AFTER > 0) {
+        var age = new Date();
+        age.setDate(age.getDate() - PURGE_AFTER);
+        var purge_date  = Utilities.formatDate(age, Session.getScriptTimeZone(), "yyyy-MM-dd");
+        search += ' before:' + purge_date;
+    }
 
     var body = [];
     var count = 0;
   
     try {
         body.push('Emails deleted: ');
-        var search = search_filter; // + ' before:' + purge_date;
+        // add the delay by uncommenting the line
         var threads = GmailApp.search(search, 0, 50);
        
         for (var i = 0; i < threads.length; i++) {

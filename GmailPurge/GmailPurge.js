714f4fc2086bcd92235f193da0f60041e7531467
Abhishek Malani <malani@gmail.com>
2016-10-27 11:54:19 -0700

    Improve readability

--- GmailPurge/GmailPurge.js
+++ GmailPurge/GmailPurge.js
@@ -5,75 +5,106 @@ specific emails.
 2. Provide authorization to access your Google a/c.
 3. Create a new project.
 4. Paste this script.
-5. Update the variables at the start of the script.
+5. Update the settings block at the start of the script.
 6. Run. 
+7. View Logger - verify working as expected.
+8. Turn off the debug flag.
 */
 
-// Where to send notifications
-var EMAIL = "";
-// Delay - skip the most recent x days of emails. Use 0 to not skip anything.
-var PURGE_AFTER = 30;
-
-// Search filter to retrieve threads.
-var search_filter = "label:label_to_delete"
-// Once you have the threads, I only wanted to delete emails sent by the following person. 
-var filter1 = { from: 'donotreply@us.gov' }
-
-// --- do not change below this ---
-
-function Email(email_subject, body) {
-    body.push("<br><br>" + MailApp.getRemainingDailyQuota() + " more emails can be sent today.");
-    var message = {
-        to: EMAIL,
-        subject: "[gscript][GmailPurge]: " + email_subject,
-        htmlBody: body.join('<br>'),
-    }
-    MailApp.sendEmail(message);
+var settings = {
+    notificationsAddr: "<your email address>",
+    olderThan: 20,                              // only touch emails older than these many days.
+    searchFilter: "label:label_to_search",      // search filter to use to look up emails (where all your CRs go)
+    filter1: { from: 'donotreply@us.gov' },     // only delete emails that match this filter. 
+    debug: true,                                // No changes, log only.
 }
 
-function purgeGmail() {
-    var search = search_filter;
-    
-    if (PURGE_AFTER > 0) {
-        var age = new Date();
-        age.setDate(age.getDate() - PURGE_AFTER);
-        var purge_date  = Utilities.formatDate(age, Session.getScriptTimeZone(), "yyyy-MM-dd");
-        search += ' before:' + purge_date;
-    }
+// -- Do not touch code below this.
 
-    var body = [];
-    var count = 0;
-  
-    try {
-        body.push('Emails deleted: ');
-        // add the delay by uncommenting the line
-        var threads = GmailApp.search(search, 0, 50);
-       
-        for (var i = 0; i < threads.length; i++) {
-            var messages = GmailApp.getMessagesForThread(threads[i]);
-            var threadSubject = threads[i].getFirstMessageSubject();
-            var threadId = threads[i].getId();
-            for (var j = 0; j < messages.length; j++) {
-                var message = messages[j];
-                var from = message.getFrom()
-                if (from.indexOf(filter1.from) != -1) {
-                    var threadTitle = 'Thread (' + threadId + '): ' + threadSubject;
-                        body.push(threadTitle);
-                        body.push("From: " + from)
-                        body.push("Date: " + message.getDate());
-                        body.push("To: " + message.getTo())
-                        body.push("");
+function GmailPurge(settings) {
+    this.settings = settings;
+}
 
-                        message.moveToTrash();
-                        count++;
+GmailPurge.prototype = {
+    constructor: GmailPurge,
+
+    notify: function(subject, body) {
+        body.push("<br><br>" + MailApp.getRemainingDailyQuota() + " more emails can be sent today.");
+        bodyText = body.join('<br>');
+        var message = {
+            to: this.settings.notificationsAddr,
+            subject: "[gscript][GmailPurge]: " + subject,
+            htmlBody: bodyText,
+        }
+        if (this.settings.debug) {
+            Logger.log(subject);
+            for (var i = 0; i < body.length; i++) {
+                Logger.log(body[i]);
+            }
+        } else {
+            MailApp.sendEmail(message);
+        }
+    },
+
+    getThreadFilter: function() {
+        var filter = this.settings.searchFilter;
+        if (this.settings.olderThan > 0) {
+            var d = new Date();
+            d.setDate(d.getDate() - this.settings.olderThan);
+            var delay = Utilities.formatDate(d, Session.getScriptTimeZone(), 'yyyy-MM-dd');
+            filter += " before:" + delay;
+        }
+        return filter;
+    },
+
+    getThreads: function() {
+        return GmailApp.search(this.getThreadFilter(), 0, 20);
+    },
+
+    run: function() {
+        var body = [];
+        var stats = { threads: 0, processed: 0 }
+
+        try {
+            body.push('Emails deleted:');
+
+            var threads = this.getThreads();
+            stats.threads = threads.length;
+
+            for (var t = 0; t < threads.length; t++) {
+                var thread = threads[t];
+                var threadId = thread.getId();
+                var threadSubject = thread.getFirstMessageSubject();
+                var messages = GmailApp.getMessagesForThread(thread);
+
+                for (var m = 0; m < messages.length; m++) {
+                    var message = messages[m];
+                    var from = message.getFrom();
+                    if (from.indexOf(this.settings.filter1.from) != -1) {
+                        body.push(
+                            'Thread (' + threadId + '): ' + threadSubject,
+                            'From: ' + from,
+                            'Date: ' + message.getDate(),
+                            'To: ' + message.getTo(),
+                            ''
+                        )
+
+                        if (!this.settings.debug) {
+                            message.moveToTrash();
+                        }
+                        stats.processed++;
+                    }
                 }
             }
         }
+        catch (ex) {
+            body.push('Exception: ' + ex.message);
+        }
+
+        var subject = 'Deleted ' + stats.processed + ' emails from ' + stats.threads + ' threads.';
+        this.notify(subject, body);
     }
-    catch (e) {
-        body.push('Exception: ' + e.message);
-    }
-  
-    var subject = 'Deleted ' + count + ' emails.';
-    Email(subject, body);  
-}
+};
+
+var purger = new GmailPurge(settings);
+purger.run();

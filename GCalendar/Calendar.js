29d0e1643e9a9da11eecc74ab62b304d92432f75
Abhishek Malani <malani@gmail.com>
2016-11-17 23:51:28 -0800

    Modularize code

--- GCalendar/Calendar.js
+++ GCalendar/Calendar.js
@@ -3,13 +3,16 @@
 
     If you use this, do let me know so I can notify you if/when I update it.
 
+// Google calendar developer API reference: 
 // https://developers.google.com/apps-script/reference/calendar/calendar
+// V3/Advanced API - used for advanced manipulation.
 // https://developers.google.com/google-apps/calendar/v3/reference/events
+// https://developers.google.com/apps-script/advanced/calendar
 
+// When run, you'll get an email like this:
 You'll get an email like this: 
 November 18, 2016 10:30:00 AM PST: <event title>
 Color updated
-
 November 21, 2016 3:00:00 PM PST: <event title> 
 Color update not needed.
 */
@@ -38,10 +41,67 @@ String.prototype.parseRFC3339Date = function() {
 
 function GCal(settings) {
     this.settings = settings;
+    this.body = [];
 }
 GCal.prototype = {
-    processEvent: function(eventId, event, calenderId) {
+    // Event processing helpers.
+    processEventHelperColorCodeByDescription: function(calendarId, eventId, event, eventStartStr, body, searchTerm, colorId) {
+        // Look for searchTerm in the description field of the event and if found, change the color of the event
+        // to the passed in colorId.
+        // See colorIds here: https://developers.google.com/apps-script/reference/calendar/color or 
+        // call Calendar.Colors.get()
+        if (event.description && event.description.indexOf(searchTerm) != -1) {
+            body.push(eventStartStr + ': ' + event.summary);
+            if (event.colorId == colorId) {
+                body.push('Color update not needed')
+            } else {
+                body.push('Color updated');
+                event.colorId = colorId;
+                if (!this.settings.debug) {
+                    Calendar.Events.patch(event, calendarId, eventId);
+                }
+            }
+        }
+    },
 
+    // Called once for each event.
+    processEvent: function(calendarId, eventId, body) {
+        // This refers to the event object mentioned here: https://developers.google.com/google-apps/calendar/v3/reference/events
+        // Note - all fields may not be present in each event so always do if (event.field) check before accessing.
+        var event = Calendar.Events.get(calendarId, eventId);
+        
+        // Event start
+        var eventStart = '', eventStartStr = '';
+        if (event.start.date) { // All day event
+            eventStart = String(event.start.date).parseRFC3339Date();
+            eventStartStr = eventStart.toLocaleDateString();
+        } else {
+            eventStart = String(event.start.dateTime).parseRFC3339Date();
+            eventStartStr = eventStart.toLocaleString();
+        }
+    
+        // Event processors:
+        // Color code events containing the passed in search term to the passed in colorId.
+        this.processEventHelperColorCodeByDescription(
+            calendarId, eventId, event, eventStartStr, body, 'dropbox.greenhouse.io', 3
+        );
+        // if (event.description) {
+        //     if (event.description.indexOf('dropbox.greenhouse.io') != -1) {
+        //         body.push(eventStartStr + ': ' + event.summary);
+        //         if (event.colorId != 3) {
+        //             body.push('Updating color');
+        //             event.colorId = 3;
+        //             if (!this.settings.debug) {
+        //                 Calendar.Events.patch(event, calendarId, eventId)
+        //             }
+        //         }
+        //         else {
+        //             body.push('Color update not needed');
+        //         }
+
+        //         body.push('')
+        //     }
+        // }
     },
 
     run: function() {
@@ -63,34 +123,8 @@ GCal.prototype = {
             if (events.items && events.items.length > 0) {
                 for (var i = 0; i < events.items.length; i++) {
                     var event = events.items[i];
-                    if (event.start.date) {
-                        // All-day event.
-                        // var start = String(event.start.date).parseRFC3339Date();
-                        // Logger.log('%s (%s)', event.summary, start.toLocaleDateString());
-                    } else {
-                        var eventId = event.id;
-                        var ev = Calendar.Events.get(calendarId, eventId);
-                        // Logger.log(ev.summary)
-                        if (ev.description) {
-                            // Logger.log(ev.summary)
-                            if (ev.description.indexOf('dropbox.greenhouse.io') != -1) {
-                                var start = String(event.start.dateTime).parseRFC3339Date();
-                                body.push(start.toLocaleString() + ': ' + ev.summary);
-                                if (ev.colorId != 3) {
-                                    body.push('Updating color');
-                                    ev.colorId = 3;
-                                    if (!this.settings.debug) {
-                                        Calendar.Events.patch(ev, calendarId, eventId)
-                                    }
-                                }
-                                else {
-                                    body.push('Color update not needed');
-                                }
-
-                                body.push('')
-                            }
-                        }
-                    }
+                    var eventId = event.id;
+                    this.processEvent(calendarId, eventId, body);
                 }
             } else {
                 body.push('No events found.');
@@ -108,7 +142,10 @@ GCal.prototype = {
     notify: function(subject, body) {
         body.push("", MailApp.getRemainingDailyQuota() + " more emails can be sent today.");
         bodyText = body.join('<br>');
-        subjectText = '[gscript][GCal]: ' + subject
+        subjectText = '[gscript][GCal]: ' + subject;
+        if (this.settings.debug) {
+            subjectText += '[debug mode]';
+        }
 
         if (this.settings.log) {
             Logger.log(subjectText);

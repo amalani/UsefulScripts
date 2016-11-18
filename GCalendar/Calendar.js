e1600d181485725364bb9d10390ad85fa0d41735
Abhishek Malani <malani@gmail.com>
2016-11-17 23:10:23 -0800

    cleanup

--- GCalendar/Calendar.js
+++ GCalendar/Calendar.js
@@ -5,12 +5,20 @@
 
 // https://developers.google.com/apps-script/reference/calendar/calendar
 // https://developers.google.com/google-apps/calendar/v3/reference/events
+
+You'll get an email like this: 
+November 18, 2016 10:30:00 AM PST: <event title>
+Color updated
+
+November 21, 2016 3:00:00 PM PST: <event title> 
+Color update not needed.
 */
 
 var settings = {
     notificationsAddr: "<enter your email address here>",   // Leave blank for no emails.
     debug: true,                                            // No changes, log only.
     log: false,                                             // Should log?
+    eventCount: 25,                                         // Process the next 'eventCount' events from now.
 }
 
 // -- Do not touch code below this.
@@ -38,8 +46,69 @@ function parseDate(string) {
 function GCal(settings) {
     this.settings = settings;
 }
-
 GCal.prototype = {
+    processEvent: function(eventId, event, calenderId) {
+
+    },
+
+    run: function() {
+        var body = [];
+        var calendarId = 'primary';         // This picks your primary calendar.
+        var now = new Date();
+
+        try {
+            // Get the next 'eventCount' events from now.
+            var events = Calendar.Events.list(calendarId, {
+                timeMin: now.toISOString(),
+                singleEvents: true,
+                orderBy: 'startTime',
+                maxResults: this.settings.eventCount,
+            });
+
+            // The API surface sometimes results in fields that may not be set. Always check
+            // whether a field exists before accessing it.
+            if (events.items && events.items.length > 0) {
+                for (var i = 0; i < events.items.length; i++) {
+                    var event = events.items[i];
+                    if (event.start.date) {
+                        // All-day event.
+                        // var start = parseDate(event.start.date);
+                        // Logger.log('%s (%s)', event.summary, start.toLocaleDateString());
+                    } else {
+                        var eventId = event.id;
+                        var ev = Calendar.Events.get(calendarId, eventId);
+                        // Logger.log(ev.summary)
+                        if (ev.description) {
+                            // Logger.log(ev.summary)
+                            if (ev.description.indexOf('dropbox.greenhouse.io') != -1) {
+                                var start = parseDate(event.start.dateTime);
+                                
+                                Logger.log('%s, (%s)', ev.summary, start.toLocaleString());
+                                
+                                if (ev.colorId != 3) {
+                                    Logger.log('Updating color');
+                                    ev.colorId = 3;
+                                    if (!this.settings.debug) {
+                                        Calendar.Events.patch(ev, calendarId, eventId)
+                                    }
+                                }
+                            }
+                            body.push('')
+                        }
+                    }
+                }
+            } else {
+                body.push('No events found.');
+            }
+        }
+        catch (ex) {
+            body.push('Exception: ' + ex.message);
+        }
+
+        var subject = "Script run results";
+        this.notify(subject, body);
+    },
+
     notify: function(subject, body) {
         body.push("", MailApp.getRemainingDailyQuota() + " more emails can be sent today.");
         bodyText = body.join('<br>');
@@ -57,58 +126,6 @@ GCal.prototype = {
             htmlBody: bodyText,
         }
         MailApp.sendEmail(message);
-    },
-
-    run: function() {
-        var body = [];
-        try {
-            var calendarId = 'primary';
-            var now = new Date();
-            var events = Calendar.Events.list(calendarId, {
-                timeMin: now.toISOString(),
-                singleEvents: true,
-                orderBy: 'startTime',
-                maxResults: 25
-            });
-
-            if (events.items && events.items.length > 0) {
-                for (var i = 0; i < events.items.length; i++) {
-                    var event = events.items[i];
-                    if (event.start.date) {
-                        // All-day event.
-                        // var start = parseDate(event.start.date);
-                        // Logger.log('%s (%s)', event.summary, start.toLocaleDateString());
-                    } else {
-                        var eventId = event.id;
-                        var ev = Calendar.Events.get(calendarId, eventId);
-                        // Logger.log(ev.summary)
-                        if (ev.description) {
-                            // Logger.log(ev.summary)
-                            if (ev.description.indexOf('dropbox.greenhouse.io') != -1) {
-                                var start = parseDate(event.start.dateTime);
-                                Logger.log('%s, (%s)', ev.summary, start.toLocaleString());
-                                if (ev.colorId != 3) {
-                                    Logger.log('Updating color');
-                                    ev.colorId = 3;
-                                    if (!this.settings.debug) {
-                                        Calendar.Events.patch(ev, calendarId, eventId)
-                                    }
-                                }
-                            }
-                            Logger.log('')
-                        }
-                    }
-                }
-            } else {
-                Logger.log('No events found.');
-            }
-        }
-        catch (ex) {
-            body.push('Exception: ' + ex.message);
-        }
-
-        var subject = "Script run complete";
-        this.notify(subject, body);
     }
 };
 

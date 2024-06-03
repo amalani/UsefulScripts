1a22a82d00d24d79fc1ee9c38e9e9617b24270ce
Abhishek Malani <malani@gmail.com>
2024-06-03 10:11:35 -0700

    Update to latest version

--- GMailDeleteOlderThan/MailUtils.js
+++ GMailDeleteOlderThan/MailUtils.js
@@ -1,16 +1,16 @@
 var MailUtils = {
-    sendEmail: function(subject, body, to, prefix) {
-      // body.push("<br><br>" + MailApp.getRemainingDailyQuota() + " more emails can be sent today.");
-  
-      if (this.config.debug) {
-        console.log("Email subject: " + subject);
-        console.log("Email sent to: " + to)
-      }
-      var message = {
-        to: to,
-        subject: "[gscript][" + prefix + "]: " + subject,
-        htmlBody: body.join(""),
-      };
-      MailApp.sendEmail(message);
+  sendEmail: function(debug, subject, body, to, prefix) {
+    // body.push("<br><br>" + MailApp.getRemainingDailyQuota() + " more emails can be sent today.");
+
+    if (debug) {
+      console.log("Email subject: " + subject);
+      console.log("Email sent to: " + to)
     }
+    var message = {
+      to: to,
+      subject: "[gscript][" + prefix + "]: " + subject,
+      htmlBody: body.join(""),
+    };
+    MailApp.sendEmail(message);
+  }
 }

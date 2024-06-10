6b0ca33d94c06478b7c99c6c845ad883c36d302f
Abhishek Malani <malani@gmail.com>
2024-06-09 22:14:02 -0700

    DateUtils - get month name

--- GMailDeleteOlderThan/DateUtils.js
+++ GMailDeleteOlderThan/DateUtils.js
@@ -1,3 +1,8 @@
+
+var DateUtils = {
+    monthNames: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+}
+
 Date.prototype.addDays = function (days) {
     const date = new Date(this.valueOf());
     date.setDate(date.getDate() + days);
@@ -9,10 +14,12 @@ Date.prototype.getLastSunday = function () {
     date.setDate(date.getDate() - date.getDay());
     return date;
 }
+
+Date.prototype.getMonthName = function() {
+    return DateUtils.monthNames[this.getMonth()];
+}
   
-  // Date.prototype.getMonthName = function() {
-  //   let monthsArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-  //   let month = new Date().getMonth();
-  //   return monthsArray[month];
-  // }
+Date.prototype.getShortMonthName = function() {
+    return this.getMonthName().substring(0, 3);
+}
   
